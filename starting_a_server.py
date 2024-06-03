dfcc482002a171aa581e61d9a438eb21b4e49f64
Gleb Mishchenko <fzovpec2@gmail.com>
2024-06-03 11:02:19 +0000

    Adding creating the directory and starting minecraft

--- starting_a_server.py
+++ starting_a_server.py
@@ -3,16 +3,17 @@ import os
 import time
 
 NUM_TRIALS = 10
-STORAGE_LATENCY = 1000000000
+STORAGE_LATENCY = 100
 FOLDER_MOUNT_PATH = "/mnt/ext4"
 MINECRAFT_PATH = 'server.jar'
 PRESENT_DIR = os.curdir
 
 current_dir = "./"
 
-def execute_and_wait(cmd):
+def execute_and_wait(cmd, timeout=10):
+    print(f'Executing "{cmd}"')
     process = pexpect.spawn('/bin/sh', ['-c', cmd], encoding='utf-8')
-    process.expect(pexpect.EOF)
+    process.expect(pexpect.EOF, timeout=timeout)
     output = process.before  # Capture the output
     process.wait()
 
@@ -27,41 +28,56 @@ def execute_and_detach(cmd):
     return child
 
 def prepare_storage():
+    print('Starting to prepare a storage')
     chmod_code, _ = execute_and_wait(f'sudo chmod +x createnullblk.sh')
     if chmod_code != 0:
         raise Exception('Could not chmod nullblk')
-
+    
+    print('Creating a memory backed block device')
     nullblk_code, nullblk = execute_and_wait(f'sudo ./createnullblk.sh 512 4096 {STORAGE_LATENCY}')
     if nullblk_code != 0:
         raise Exception('Could not create nullblock space')
     
+    nullblk = nullblk.strip()
     dev_path = f'/dev/{nullblk}'
-
-    mkfs_code, _ = execute_and_wait(f'sudo mkfs.ext4 {dev_path}')
+    print('Creating a file system on memory backed storage device')
+    
+    mkfs_code, output = execute_and_wait(f'sudo mkfs.ext4 {dev_path}', 90)
     if mkfs_code != 0:
+        print(output)
         raise Exception('Could not mark the space for file system')
     
-    mkdir_code, _ = execute_and_wait(f'sudo mkdir /mnt/ext4')
-    if mkdir_code != 0:
-        raise Exception('Could not make /mnt/ext4 folder')
+    print('Creating a root folder')
+
+    root_fpath = '/mnt/ext4'
     
-    mount_code, _ = execute_and_wait(f'sudo mount {dev_path} /mnt/ext4')
+    if not os.path.exists(root_fpath):
+        mkdir_code, output = execute_and_wait(f'sudo mkdir {root_fpath}')
+        if mkdir_code != 0:
+            print(output)
+            raise Exception('Could not make /mnt/ext4 folder')
+    
+    print('Mounting a block device onto a root folder')
+    mount_code, _ = execute_and_wait(f'sudo mount {dev_path} /mnt/ext4', 60)
     if mount_code != 0:
         raise Exception('Could not mount folder')
-
-    cp_code, _ = execute_and_wait(f'sudo cp -r {PRESENT_DIR} {dev_path}')
+    
+    print('Copying Minecraft into a new folder')
+    cp_code, output = execute_and_wait(f'sudo cp -r {PRESENT_DIR} {root_fpath}', 60)
     if cp_code != 0:
+        print(output)
         raise Exception('Could not run copy into marked directory')
     
-    os.chdir(dev_path)
+    os.chdir(root_fpath)
 
 def start_minecraft():
+    print('Starting Minecraft')
     return execute_and_detach(f'sudo java -jar {MINECRAFT_PATH}')
 
 prepare_storage()
 start_time = time.time()
 minecraft = start_minecraft()
-minecraft.expect('[Server thread/INFO]: Done', timeout=60)
+minecraft.expect('Done', timeout=60)
 end_time = time.time()
 
-print(start_time - end_time)
+print(end_time-start_time)
